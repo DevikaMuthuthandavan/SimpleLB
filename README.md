1ecd45007d0e59931f28f3985041a2bbcc05f20b
Devika <spmdevika@gmail.com>
2023-09-10 14:03:46 +0530

    Adding list of backend servers

--- README.md
+++ README.md
@@ -4,8 +4,16 @@ A Simple Load Balancer using go lang.
 To create a server in Python:
 
 python -m venv pyvenv --Create a virtual env
+source pyvenv/bin/activate -- To activate the created venv
 pip install flask -- Install flask
 python3 server.py  -- To run the server
 
 -------------------------------------------------------------------------------------------------------------------
+To run Load Balancer:
 
+go run main.go
+
+-------------------------------------------------------------------------------------------------------------------
+To run server.py
+source pyvenv/bin/activate
+python3 server.py "ServerName" "portNo" --Eg., "Server 1" "5001"
--- main.go
+++ main.go
@@ -4,6 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+var (
+	serverlist = []string{
+		"http://127.0.0.1:5000",
+		"http://127.0.0.1:5001",
+		"http://127.0.0.1:5002",
+		"http://127.0.0.1:5003",
+		"http://127.0.0.1:5004",
+	}
+
+	lastServedIdx = 0
 )
 
 func main() {
@@ -13,5 +27,15 @@ func main() {
 }
 
 func forwardRequest(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "Hello from load balancer")
+	url := getServer()
+	rProxy := httputil.NewSingleHostReverseProxy(url)
+	rProxy.ServeHTTP(res, req)
+	//fmt.Fprintln(res, "Hello from load balancer")
+}
+
+func getServer() *url.URL {
+	serverUrl := serverlist[(lastServedIdx+1)%5]
+	url, _ := url.Parse(serverUrl)
+	lastServedIdx++
+	return url
 }
