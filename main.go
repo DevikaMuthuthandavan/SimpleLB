f2a6169ec1ab304b59daad64ca86fccf460a4300
Devika <spmdevika@gmail.com>
2023-09-12 21:21:51 +0530

    Working code

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 
 var (
 	serverlist = []*server{
-		newServer("http://127.0.0.1:5000"),
-		newServer("http://127.0.0.1:5001"),
-		newServer("http://127.0.0.1:5002"),
-		newServer("http://127.0.0.1:5003"),
-		newServer("http://127.0.0.1:5004"),
+		newServer("server-1", "http://127.0.0.1:5000"),
+		newServer("server-1", "http://127.0.0.1:5001"),
+		newServer("server-1", "http://127.0.0.1:5002"),
+		newServer("server-1", "http://127.0.0.1:5003"),
+		newServer("server-1", "http://127.0.0.1:5004"),
 	}
 
 	lastServedIdx = 0
@@ -21,23 +21,32 @@ var (
 func main() {
 	fmt.Println("Simple Load Balancer")
 	http.HandleFunc("/", forwardRequest)
+	go startHealthCheck()
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
 func forwardRequest(res http.ResponseWriter, req *http.Request) {
-	s := getServer()
-	s.ReverseProxy.ServeHTTP(res, req)
-	//fmt.Fprintln(res, "Hello from load balancer")
+	server, err := getHealthyServer()
+	if err != nil {
+		http.Error(res, "Couldn't process request: "+err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	server.ReverseProxy.ServeHTTP(res, req)
+}
+
+func getHealthyServer() (*server, error) {
+	for i := 0; i < len(serverlist); i++ {
+		server := getServer()
+		if server.IsHealthy {
+			return server, nil
+		}
+	}
+	return nil, fmt.Errorf("no healthy hosts")
 }
 
 func getServer() *server {
 	nextIndex := (lastServedIdx + 1) % len(serverlist)
-	s := serverlist[nextIndex]
+	server := serverlist[nextIndex]
 	lastServedIdx = nextIndex
-	return s
+	return server
 }
-
-// func createHost(urlstr string) *httputil.ReverseProxy {
-// 	u, _ := url.Parse(urlstr)
-// 	return httputil.NewSingleHostReverseProxy(u)
-// }
--- server.go
+++ server.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 )
 
 type server struct {
-	Url          string
+	Name         string
+	URL          string
 	ReverseProxy *httputil.ReverseProxy
 	IsHealthy    bool
 }
 
-func newServer(urlstr string) *server {
+func newServer(name string, urlstr string) *server {
 	u, _ := url.Parse(urlstr)
 	rp := httputil.NewSingleHostReverseProxy(u)
 	return &server{
-		Url:          urlstr,
+		Name:         name,
+		URL:          urlstr,
 		ReverseProxy: rp,
 		IsHealthy:    true,
 	}
+}
+
+func (s *server) checkHealth() bool {
+	resp, err := http.Head(s.URL)
+	if err != nil {
+		s.IsHealthy = false
+		return s.IsHealthy
+	}
+	if resp.StatusCode != http.StatusOK {
+		s.IsHealthy = false
+		return s.IsHealthy
+	}
+	s.IsHealthy = true
+	return s.IsHealthy
 }
